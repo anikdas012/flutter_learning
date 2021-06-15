24c47e541dc82c1287467831ca2a550a68a65234
Anik Das <anikdas012@gmail.com>
2021-06-15 19:52:11 +0600

    Add new widget for descript text

--- lib/intro_slider/intro_screen.dart
+++ lib/intro_slider/intro_screen.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class IntroDescription extends StatelessWidget {
+
+  final String _description;
+
+  IntroDescription(this._description);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _description,
+      textAlign: TextAlign.center,
+      softWrap: true,
+      style: const TextStyle(
+        color: Colors.grey,
+        fontSize: 13.0,
+      ),
+    );
+  }
+}
 
 class IntroImage extends StatelessWidget {
 
