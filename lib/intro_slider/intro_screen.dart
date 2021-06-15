561ccd1fb90ca747d5fad2204cd0c27b3876046b
Anik Das <anikdas012@gmail.com>
2021-06-15 19:52:50 +0600

    Add two new widgets for title text
     - one normal text widget
     - another rich text widget

--- lib/intro_slider/intro_screen.dart
+++ lib/intro_slider/intro_screen.dart
@@ -1,6 +1,70 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class IntroTitle extends StatelessWidget {
+
+  final String _title;
+
+  IntroTitle(this._title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _title,
+      textAlign: TextAlign.center,
+      softWrap: true,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 18.0,
+      ),
+    );
+  }
+}
+
+class IntroRichTitle extends StatelessWidget {
+
+  final String _title;
+
+  IntroRichTitle(this._title);
+
+  @override
+  Widget build(BuildContext context) {
+    var titles = _title.split('\n');
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: titles[0],
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
+          ),
+          TextSpan(
+            text: '\n',
+          ),
+          TextSpan(
+            text: 'and',
+            style: const TextStyle(
+              color: Colors.orange,
+              fontSize: 18.0,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          TextSpan(
+            text: titles[1].replaceAll('and', ''),
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
+
 class IntroDescription extends StatelessWidget {
 
   final String _description;
