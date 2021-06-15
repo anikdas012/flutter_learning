97f1c0ada2863af861671f746f2ca733e0c2050f
Anik Das <anikdas012@gmail.com>
2021-06-15 23:17:30 +0600

    Create new StatefulWidget for intro sliding screens

---
+++ lib/intro_slider/intro_slider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'intro_slider_state.dart';
+
+class IntroScreen extends StatefulWidget {
+  IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  IntroScreenState createState() => new IntroScreenState();
+}
