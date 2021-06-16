c9828a4ed041147d8eb8acb95d1a5e5f75087a52
Anik Das <anikdas012@gmail.com>
2021-06-16 18:08:22 +0600

    Add new widget for password input.
     - its a statefulwidget
     - show/hide password are two state

--- lib/log_in/log_in.dart
+++ lib/log_in/log_in.dart
@@ -37,6 +37,69 @@ class LogInUsernameWidget extends StatelessWidget {
   }
 }
 
+class LogInPasswordWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LogInPasswordState();
+  }
+}
+
+class LogInPasswordState extends State<LogInPasswordWidget> {
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = true;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Theme(
+      data: new ThemeData(
+        // primarySwatch: Colors.orange,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.orange,
+            ),
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.orange,
+            ),
+          ),
+          labelText: "Password",
+          labelStyle: TextStyle(
+            color: Colors.orange[700],
+          ),
+          contentPadding: EdgeInsets.zero,
+          suffixIcon: IconButton(
+            icon: Icon(
+              _passwordVisible ? Icons.visibility : Icons.visibility_off,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              setState(() {
+                _passwordVisible = !_passwordVisible;
+              });
+            },
+          ),
+        ),
+        obscureText: _passwordVisible,
+        cursorColor: Colors.orange,
+        cursorHeight: 20.0,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+        ),
+      ),
+    );
+  }
+
+}
+
 class LogInImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
