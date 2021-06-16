9ced2d085605713a51d06a3b1dbfbda82935056e
Anik Das <anikdas012@gmail.com>
2021-06-16 18:07:23 +0600

    Create new widget for username input

--- lib/log_in/log_in.dart
+++ lib/log_in/log_in.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+class LogInUsernameWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Theme(
+        data: new ThemeData(
+          // primarySwatch: Colors.orange,
+        ),
+        child: TextFormField(
+          decoration: InputDecoration(
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.orange,
+              ),
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.orange,
+              ),
+            ),
+            labelText: "Email",
+            labelStyle: TextStyle(
+              color: Colors.orange[700],
+            ),
+            contentPadding: EdgeInsets.zero,
+          ),
+          cursorColor: Colors.orange,
+          cursorHeight: 20.0,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+    );
+  }
+}
+
 class LogInImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
